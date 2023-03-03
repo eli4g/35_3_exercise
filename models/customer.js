6821bba0fdf1e94cdeaaede48c8347602d35fbe0
eli4g <cgremont@gmail.com>
2023-03-02 20:35:50 -0500

    initial commit

---
+++ models/customer.js
@@ -0,0 +1,129 @@
+/** Customer for Lunchly */
+
+const db = require("../db");
+const Reservation = require("./reservation");
+
+/** Customer of the restaurant. */
+
+class Customer {
+  constructor({ id, firstName, lastName, phone, notes }) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.phone = phone;
+    this.notes = notes;
+  }
+
+  /** find all customers. */
+
+  static async all() {
+    const results = await db.query(
+      `SELECT id, 
+         first_name AS "firstName",  
+         last_name AS "lastName", 
+         phone, 
+         notes
+       FROM customers
+       ORDER BY last_name, first_name`
+    );
+    return results.rows.map(c => new Customer(c));
+  }
+
+  /** get a customer by ID. */
+
+  static async get(id) {
+    const results = await db.query(
+      `SELECT id, 
+         first_name AS "firstName",  
+         last_name AS "lastName", 
+         phone, 
+         notes 
+        FROM customers WHERE id = $1`,
+      [id]
+    );
+
+    const customer = results.rows[0];
+
+    if (customer === undefined) {
+      const err = new Error(`No such customer: ${id}`);
+      err.status = 404;
+      throw err;
+    }
+
+    return new Customer(customer);
+  }
+
+  /** get all reservations for this customer. */
+
+  async getReservations() {
+    return await Reservation.getReservationsForCustomer(this.id);
+  }
+
+  /** save this customer. */
+
+  async save() {
+    if (this.id === undefined) {
+      const result = await db.query(
+        `INSERT INTO customers (first_name, last_name, phone, notes)
+             VALUES ($1, $2, $3, $4)
+             RETURNING id`,
+        [this.firstName, this.lastName, this.phone, this.notes]
+      );
+      this.id = result.rows[0].id;
+    } else {
+      await db.query(
+        `UPDATE customers SET first_name=$1, last_name=$2, phone=$3, notes=$4
+             WHERE id=$5`,
+        [this.firstName, this.lastName, this.phone, this.notes, this.id]
+      );
+    }
+  }
+
+  static async filter(search){
+
+   
+    const results = await db.query(
+      `SELECT id, 
+         first_name AS "firstName",  
+         last_name AS "lastName", 
+         phone, 
+         notes
+       FROM customers
+       WHERE (LOWER(first_name) LIKE LOWER($1) or LOWER(last_name) LIKE LOWER($2))
+       ORDER BY last_name, first_name`, ['%' + search + '%','%' + search + '%']
+    );
+    return results.rows.map(c => new Customer(c));
+  }
+
+  
+  static async top(){
+
+    const results = await db.query(
+      `SELECT customers.id AS id, 
+         first_name AS "firstName",  
+         last_name AS "lastName", 
+         phone, 
+         customers.notes AS notes
+         
+       FROM customers INNER JOIN reservations on customers.id = reservations.customer_id
+       GROUP BY customers.id
+       ORDER BY count(reservations.id) DESC
+       LIMIT 10`
+    );
+    return results.rows.map(c => new Customer(c));
+
+
+
+  }
+
+
+  fullName(){
+
+    return (`${this.firstName} ${this.lastName}`)
+
+  }
+
+
+}
+
+module.exports = Customer;
